396244a66526e1109e005f8188716ad52f1bd3bf
Pavan2702 <kaushikpatil42161@gmail.com>
2024-03-13 12:49:46 +0530

    Add files

---
+++ React/CRUD Opration/src/App.jsx
@@ -0,0 +1,12 @@
+import Form from './2.Formvalidation/Fromik/Form'
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
--- React/React Practice/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import AppRedux from "./2.RTK-Query/AppRedux";
-
-export default  function App() {
-  return (
-    <>
-<AppRedux/>
-    </>
-  )
-}
