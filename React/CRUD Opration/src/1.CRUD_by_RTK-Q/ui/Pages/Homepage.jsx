4bc91a469c019ad41842968166fef9fa8f82a079
Pavan2702 <kaushikpatil42161@gmail.com>
2024-03-20 15:21:06 +0530

    File modified

--- React/CRUD Opration/src/1.CRUD_by_RTK-Q/ui/Pages/Homepage.jsx
+++ React/CRUD Opration/src/1.CRUD_by_RTK-Q/ui/Pages/Homepage.jsx
@@ -68,7 +68,7 @@ export default function Homepage() {
                     <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
                 </div>
             ) : (
-                <div>
+                <div className="">
                     <div className="flex justify-center">
                         <button
                             className="cursor-pointer font-semibold overflow-hidden rounded-md relative z-100 border border-green-500 group px-8 py-2"
@@ -81,9 +81,9 @@ export default function Homepage() {
                             <span className="absolute w-full h-full bg-green-500 -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
                         </button>
                     </div>
-                    <div className="flex items-center justify-center p-3">
+                    <div className="flex items-center justify-center p-7">
                         <input
-                            class="bg-zinc-200 text-zinc-600 font-mono ring-1 ring-zinc-400 focus:ring-2 focus:ring-rose-400 outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-full px-4 py-1 shadow-md focus:shadow-lg focus:shadow-rose-400"
+                            class="bg-zinc-200 text-zinc-600 font-mono ring-1 ring-zinc-400 focus:ring-2 focus:ring-green-400 outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-full px-4 py-1 shadow-md focus:shadow-lg focus:shadow-green-400"
                             autocomplete="off"
                             placeholder="TailwindCSS..."
                             name="text"
